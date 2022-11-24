5f50fb20b4f484abf2c288b54b11a676088e2a2a
Kimsolchan <khj25830@naver.com>
2022-11-24 21:44:34 +0900

    호텔 프로젝트 entity-Room 첫 번째 작업

--- src/main/java/com/team/grabjava/hotel/entity/Room.java
+++ src/main/java/com/team/grabjava/hotel/entity/Room.java
@@ -1,5 +1,28 @@
 package com.team.grabjava.hotel.entity;
 
 public class Room {
-    // 작업 시작합니다.
+    // 맴버변수 : 객실번호, 방크기, 객실비용
+    private final int roomNo;
+    private final int size;
+    private final int price;
+
+    // 생성자 : 값을 받아서 맴버변수에 저장
+    public Room(int roomNo, int size, int price){
+        this.roomNo = roomNo;
+        this.size = size;
+        this.price = price;
+    }
+
+    // getter 매소드 : 저장하고 있는 객실정보를 돌려준다?
+    public int getRoomNo(){
+        return roomNo;
+    }
+
+    public int getRoomSize(){
+        return size;
+    }
+
+    public int getRoomPrice(){
+        return price;
+    }
 }
