e3ef105d574275f0ccc9fe3a2a1cea521c35cec4
Kimsolchan <khj25830@naver.com>
2022-11-24 22:46:06 +0900

    작업]  솔찬 InputUserData 작업 미완성(피드백에 따라 수정할 수도 있습니다.)

--- src/main/java/com/team/grabjava/hotel/service/personal_service/SolchanService.java
+++ src/main/java/com/team/grabjava/hotel/service/personal_service/SolchanService.java
@@ -1,4 +1,24 @@
-package main.java.com.team.grabjava.hotel.service.personal_service;
+package com.team.grabjava.hotel.service.personal_service;
 
 public class SolchanService {
+    private String userName;
+    private String userPhone;
+    private int userAsset;
+
+    public SolchanService(String userName, String userPhone, int userAsset){
+        this.userName = userName;
+        this.userPhone = userPhone;
+        this.userAsset = userAsset;
+    }
+
+    // 클라이언트가 입력한 유저 정보를 확인하여 맞으면 DB로 보내고 틀리면 오류값을 클라이언트로 보내는 메서드?
+    public boolean inputUserData(){
+        // front 정보 입력 요청 -> 클라이언트 정보 입력 -> 입력한 정보는 여기로 와서 조건을 거쳐 다음 절차로 진행?
+        // 이름이 비었거나 , 소지금을 음수로 입력했다면
+        if(userName == " " || userAsset <= 0){
+            return false;
+        }else{
+            return true;
+        }
+    }
 }
