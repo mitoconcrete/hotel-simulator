18250f6f5dd41232dc66de0627d28cc0757e75ac
seongjun <seongjuni1@naver.com>
2022-11-25 15:39:47 +0900

    생성] 프레젠테이션 셀렉트서비스인터페이스 생성완료

--- src/main/java/com/team/grabjava/hotel/presentation/SelectServiceInterface.java
+++ src/main/java/com/team/grabjava/hotel/presentation/SelectServiceInterface.java
@@ -1,4 +1,13 @@
 package com.team.grabjava.hotel.presentation;
 
 public class SelectServiceInterface {
+
+    public void startScanner(){
+        System.out.println("1. 예약");
+        System.out.println("2. 예약번호 조회");
+        System.out.println("3. 예약내역 조회");
+        System.out.println("4. 예약 취소");
+        System.out.println("5. 모든 예약내역 조회(관리자)");
+        System.out.println("원하시는 서비스를 선택해 주세요 : ");
+    }
 }
