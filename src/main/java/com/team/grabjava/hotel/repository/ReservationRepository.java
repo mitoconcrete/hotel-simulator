8535f39a9191922910a9fd8b509da0468c8a4ba2
Kimsolchan <khj25830@naver.com>
2022-11-25 23:59:12 +0900

    생성] reservationrepository - createReservation 매서드 완성

--- src/main/java/com/team/grabjava/hotel/repository/ReservationRepository.java
+++ src/main/java/com/team/grabjava/hotel/repository/ReservationRepository.java
@@ -1,9 +1,12 @@
 package com.team.grabjava.hotel.repository;
 
 import com.team.grabjava.hotel.entity.Reservation;
+import com.team.grabjava.hotel.entity.Room;
+import com.team.grabjava.hotel.entity.User;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class ReservationRepository {
     // 예약 리스트
@@ -11,8 +14,17 @@ public class ReservationRepository {
 
 
     // 예약리스트 조회(getReservationList)
-    public List<Reservation> getReservationList(){
+    public List<Reservation> getReservationList() {
         return reservationList;
     }
+
+    // 예약을 생성해서 예약리스로 넣는다.(createReservation)
+    public Reservation createReservation(Room room, String userName, String userPhone, String reservationDate) {
+        // 생성자 생성과 동시에  reservationList에 넘갔다.
+        Reservation reservation = new Reservation(room, userName, userPhone, reservationDate);
+        reservationList.add(reservation);
+        return reservation;
+    }
+
 
 }
