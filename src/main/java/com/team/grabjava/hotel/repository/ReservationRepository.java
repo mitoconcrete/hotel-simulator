1608c9f748d344b86231d7667ad505c4629868bb
Kimsolchan <khj25830@naver.com>
2022-11-25 22:46:08 +0900

    생성]  getReservationList 매서드 완성

--- src/main/java/com/team/grabjava/hotel/repository/ReservationRepository.java
+++ src/main/java/com/team/grabjava/hotel/repository/ReservationRepository.java
@@ -1,4 +1,18 @@
 package com.team.grabjava.hotel.repository;
 
+import com.team.grabjava.hotel.entity.Reservation;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ReservationRepository {
+    // 예약 리스트
+    List<Reservation> reservationList = new ArrayList<>();
+
+
+    // 예약리스트 조회(getReservationList)
+    public List<Reservation> getReservationList(){
+        return reservationList;
+    }
+
 }
