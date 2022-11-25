3e8e5f2c80fe88cbdb56a8d21fc7fe99cedbe27f
seongjun <seongjuni1@naver.com>
2022-11-25 15:33:16 +0900

    생성] 서비스 리퀘스트리설베이션캔슬 생성완료

--- src/main/java/com/team/grabjava/hotel/service/personal_service/SungjunService.java
+++ src/main/java/com/team/grabjava/hotel/service/personal_service/SungjunService.java
@@ -59,4 +59,15 @@ public class SungjunService {
         return reservation;
     }
 
+
+    public boolean requestReservationCancel(String reservationId){
+        for (Reservation r : reservationRepository.getReservationList()) {
+            if(r.getId().equals(reservationId)){
+                reservationRepository.deleteReservation(r);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
