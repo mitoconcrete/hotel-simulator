6101af1d3a3803e341df7f92ba0473af94f674f6
seongjun <117061026+acejun9@users.noreply.github.com>
2022-11-25 16:43:11 +0900

    생성] 서비스 체크데이트포맷 생성완료 (#15)
    * 생성] 서비스 겟룸리스트 생성완료
    
    * 생성] 서비스 체크데이트포맷 생성완료

--- src/main/java/com/team/grabjava/hotel/service/personal_service/SungjunService.java
+++ src/main/java/com/team/grabjava/hotel/service/personal_service/SungjunService.java
@@ -1,4 +1,52 @@
 package com.team.grabjava.hotel.service.personal_service;
 
+import com.team.grabjava.hotel.entity.Hotel;
+import com.team.grabjava.hotel.entity.Reservation;
+import com.team.grabjava.hotel.entity.Room;
+import com.team.grabjava.hotel.repository.HotelRepository;
+import com.team.grabjava.hotel.repository.ReservationRepository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SungjunService {
+    private HotelRepository hotelRepository = new HotelRepository();
+    private ReservationRepository reservationRepository = new ReservationRepository();
+
+    public List<Room> getRoomList(String date){
+        List<Room> list = new ArrayList<>();
+
+        for(int i=0;i<hotelRepository.getRoomList().size();i++){
+            list.add(hotelRepository.getRoomList().get(i));
+        }
+
+        for(Room room : hotelRepository.getRoomList()) {
+            for (Reservation reservation : reservationRepository.getReservationList()) {
+                String reservationDate = reservation.getReservationDate().toString().substring(0, 10);
+                if (reservationDate.equals(date) && reservation.getRoomNo()==room.getRoomNo()) {
+                    list.remove(room);
+                    break;
+                }
+            }
+        }
+
+        return list;
+    }
+
+    boolean checkDateFormat(String date) {
+        if (date.length() == 10 && date.contains("-")) {
+            date = date.replace("-", "");
+            if (date.length() == 8) {
+                for (int i = 0; i < 8; i++) {
+                    if ((int) date.charAt(i) > 57 || (int) date.charAt(i) < 48) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
