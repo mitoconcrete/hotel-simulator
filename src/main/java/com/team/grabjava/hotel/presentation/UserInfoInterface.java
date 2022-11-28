e51146e16d16037fd09e5bb1a7cf1a518dfc904d
Tae hoon Kim <62238955+mitoconcrete@users.noreply.github.com>
2022-11-28 11:39:48 +0900

    수정] 콜론 추가하여 input 부분을 명시적으로 표현합니다.

--- src/main/java/com/team/grabjava/hotel/presentation/UserInfoInterface.java
+++ src/main/java/com/team/grabjava/hotel/presentation/UserInfoInterface.java
@@ -4,6 +4,6 @@ import com.team.grabjava.hotel.entity.Reservation;
 
 public class UserInfoInterface {
     public void startScanner(){
-        System.out.println("정보를 입력해주세요." + System.lineSeparator() + "(이름 전화번호 소지금)");
+        System.out.println("정보를 입력해주세요." + System.lineSeparator() + "(이름 전화번호 소지금) :");
     }
 }
