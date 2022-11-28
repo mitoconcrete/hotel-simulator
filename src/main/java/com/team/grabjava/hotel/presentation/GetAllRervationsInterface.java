cf7cc406c64f54b0e0b7126b07be74ea22ae627c
mitoconcrete <xogns98@gmail.com>
2022-11-28 12:54:52 +0900

    생성] 모든 예약내역을 조회하는 인터페이스를 만듭니다.

--- src/main/java/com/team/grabjava/hotel/presentation/GetAllRervationsInterface.java
+++ src/main/java/com/team/grabjava/hotel/presentation/GetAllRervationsInterface.java
@@ -1,4 +1,21 @@
 package com.team.grabjava.hotel.presentation;
 
+import com.team.grabjava.hotel.entity.Reservation;
+
+import java.util.List;
+
 public class GetAllRervationsInterface {
+    public void startScanner(){
+        System.out.println("관리자 비밀번호를 입력하세요. :");
+    }
+
+    public void showAllReservationsMessage(List<Reservation> reservations){
+        for(Reservation reservation: reservations){
+            System.out.println(reservation.getId() + " / " + reservation.getRoom().getRoomNo() + " / " + reservation.getUserName() + " / " + reservation.getUserPhone() + " / " + reservation.getReservationDate());
+        }
+    }
+
+    public void showNotExistReservationsMessage(){
+        System.out.println("예약내역이 없습니다.");
+    }
 }
